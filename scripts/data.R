3c5d222d0bbb2c5a8eef83ca713e84b31e4b196d
Tom Elliott <tomelliottnz@gmail.com>
2016-12-05 10:15:52 +1300

    document SURF data

--- scripts/data.R
+++ scripts/data.R
@@ -1,4 +1,9 @@
 ## Script to create(/update) datasets shipped with iNZight
 
 SURFIncomeSurvey <- read.csv("~/Downloads/SURFIncomeSurvey.csv", header = TRUE, comment.char = "#")
-devtools::use_data(SURFIncomeSurvey)
+SURFIncomeSurvey$X <- NULL
+devtools::use_data(SURFIncomeSurvey, overwrite = TRUE)
+
+
+devtools::load_all()
+devtools::document()
