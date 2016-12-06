3810e50b5bd2271d58631ab7e55fa315732002c3
Tom Elliott <tomelliottnz@gmail.com>
2016-12-06 16:01:23 +1300

    append _200 to SURF data name

--- scripts/data.R
+++ scripts/data.R
@@ -1,8 +1,8 @@
 ## Script to create(/update) datasets shipped with iNZight
 
-SURFIncomeSurvey <- read.csv("~/Downloads/SURFIncomeSurvey.csv", header = TRUE, comment.char = "#")
-SURFIncomeSurvey$X <- NULL
-devtools::use_data(SURFIncomeSurvey, overwrite = TRUE)
+SURFIncomeSurvey_200 <- read.csv("~/Downloads/SURFIncomeSurvey.csv", header = TRUE, comment.char = "#")
+SURFIncomeSurvey_200$X <- NULL
+devtools::use_data(SURFIncomeSurvey_200, overwrite = TRUE)
 
 
 devtools::load_all()
