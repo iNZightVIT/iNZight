059364322a05fae4ea7704c72d65a02a82345c46
yiwen16 <45804413+yiwen16@users.noreply.github.com>
2019-03-27 15:02:52 +1300

    Ignore RData and dev script

--- scripts/datetimes.R
+++ scripts/datetimes.R
@@ -1,14 +1,19 @@
-#install_deps("~/iNZightTools")
-#install_deps("~/iNZight", dependencies = TRUE)
+install_deps("~/iNZightTools", dependencies = TRUE)
+install_deps("~/iNZight", dependencies = TRUE)
+install_deps("~/iNZightPlots", dependencies = TRUE)
+
+install.packages("glue")
 
 library(devtools)
 
+# install_deps("~/iNZightPlots")
 load_all("~/iNZightTools")
+load_all("~/iNZightPlots")
 load_all("~/iNZight")
 
 document("~/iNZightTools")
 
-data = readr::read_csv("C:\\Users\\Yiwen\\Documents\\iNZight\\scripts\\paris1.csv")
+data = readr::read_csv("C:\\Users\\30576\\Documents\\iNZight\\scripts\\paris1.csv")
 
 ## Convert function
 try(dispose(kk$win), TRUE)
