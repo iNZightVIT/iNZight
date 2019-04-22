341f94e08bad806cd96cd2d6a0632923a793baed
Tom Elliott <tomelliottnz@gmail.com>
2019-04-23 09:39:35 +1200

    Fix indentation

--- R/iNZLoadSave.R
+++ R/iNZLoadSave.R
@@ -11,29 +11,38 @@ iNZLoadSaveWin <-
             initialize = function(gui, action) {
                 initFields(GUI = gui, action = action)
 
-                init.d <- file.path("~", "Documents", "iNZightVIT", "Saved Data")
+                init.d <- file.path(
+                    "~", "Documents", "iNZightVIT", "Saved Data"
+                )
 
                 ## select a file name:
                 filename <<- gfile("Select file",
-                                   type = switch(action,
-                                                 "load" = "open",
-                                                 "save" = "save"),
-                                   filter = list(
-                                       "iNZight Save Files (.rds)" = list(patterns = "*.rds"),
-                                       "All Files" = list(patterns = "*")),
-                                   parent = gui$win,
-                                   initial.dir = path.expand(init.d))
+                    type = switch(action,
+                        "load" = "open",
+                        "save" = "save"
+                    ),
+                    filter = list(
+                        "iNZight Save Files (.rds)" = list(patterns = "*.rds"),
+                        "All Files" = list(patterns = "*")
+                    ),
+                    parent = gui$win,
+                    initial.dir = path.expand(init.d)
+                )
 
                 ## load/save the file
                 if (length(filename))
                     switch(action,
-                           "load" = {
-                               gui$setDocument(iNZDocument$new(data = readRDS(filename)))
-                           },
-                           "save" = {
-                               if (!grepl(".rds", filename)) filename <<- paste0(filename, ".rds")
-                               saveRDS(gui$getActiveData(), filename)
-                           })
+                        "load" = {
+                            gui$setDocument(
+                                iNZDocument$new(data = readRDS(filename))
+                            )
+                        },
+                        "save" = {
+                            if (!grepl(".rds", filename)) 
+                                filename <<- paste0(filename, ".rds")
+                            saveRDS(gui$getActiveData(), filename)
+                        }
+                    )
 
                 return(invisible(NULL))
             }
