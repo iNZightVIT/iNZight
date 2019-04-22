b85f4a3c46c8c5e184cde8413bfea80eeae4b368
Tom Elliott <tomelliottnz@gmail.com>
2019-04-23 11:50:56 +1200

    Remove expect on load function

--- tests/testthat/test_rds.R
+++ tests/testthat/test_rds.R
@@ -5,9 +5,9 @@ ui$initializeGui()
 on.exit(gWidgets2::dispose(ui$win))
 
 test_that("Strings are converted to factor", {
-    expect_silent(iNZLoadSaveWin$new(ui, "load", "test.rds"))
-    # expect_equal(ui$getActiveData()$x, 1:5)
-    # expect_equal(ui$getActiveData()$y, as.factor(LETTERS[1:5]))
+    iNZLoadSaveWin$new(ui, "load", "test.rds")
+    expect_equal(ui$getActiveData()$x, 1:5)
+    expect_equal(ui$getActiveData()$y, as.factor(LETTERS[1:5]))
 })
 
 test_that("Saving as RDS works", {
