55094b54e68f4981f005066428701da304aa5a41
Tom Elliott <tomelliottnz@gmail.com>
2019-04-23 13:05:14 +1200

    Use all temp files

--- tests/testthat/test_rds.R
+++ tests/testthat/test_rds.R
@@ -4,19 +4,28 @@ ui <- iNZGUI$new()
 ui$initializeGui()
 on.exit(gWidgets2::dispose(ui$win))
 
-test_that("Strings are converted to factor", {
-    iNZLoadSaveWin$new(ui, "load", "test.rds")
-    # expect_equal(ui$getActiveData()$x, 1:5)
-    # expect_equal(ui$getActiveData()$y, as.factor(LETTERS[1:5]))
+tmp <- tempfile(fileext = ".rds")
+on.exit(unlink(tmp), add = TRUE)
+
+d <- data.frame(x = 1:5, y = LETTERS[1:5])
+saveRDS(d, tmp)
+
+test_that("Data can be saved as an RDS", {
+    tmp2 <- tempfile(fileext = ".rds")
+    on.exit(unlink(tmp2))
+
+    iNZLoadSaveWin(ui, "save", tmp2)
+    expect_equal(
+        readRDS(tmp2),
+        ui$getActiveData()
+    )
 })
 
-test_that("Saving as RDS works", {
-    # tmp <- tempfile(fileext = ".rds")
-    # on.exit(unlink(tmp))
+test_that("Data can be loaded from RDS", {
+    iNZLoadSaveWin$new(ui, "load", tmp)
+    expect_equal(dim(ui$getActiveData()), c(5, 2))
+})
 
-    # iNZLoadSaveWin(ui, "save", tmp)
-    # expect_equal(
-    #     readRDS(tmp),
-    #     ui$getActiveData()
-    # )
+test_that("Strings are converted to factor", {
+    expect_is(ui$getActiveData()$y, "factor")
 })
