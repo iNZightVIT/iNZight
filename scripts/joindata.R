9acda677f5d5cffc30caf0a757ca036545b2ec94
yiwen16 <yhe443@aucklanduni.ac.nz>
2019-04-29 15:16:52 +1200

    join now returns an error when joining columns have different types

--- scripts/joindata.R
+++ scripts/joindata.R
@@ -8,7 +8,7 @@ load_all("~/iNZightTools")
 load_all("~/iNZight")
 
 
-data = readr::read_csv("scripts/Join2.csv")
+data2 = readr::read_csv("scripts/Join2.csv")
 data
 
 try(dispose(kk$win), TRUE)
@@ -23,3 +23,18 @@ data = readr::read_csv("C:\\Users\\Yiwen\\Documents\\parisjoin.csv") #original
 data2 = readr::read_csv("C:\\Users\\Yiwen\\Documents\\parisjoin2.csv") #has the same column "Instagram photo" with completely different values
 data3 = readr::read_csv("C:\\Users\\Yiwen\\Documents\\parisjoin3.csv") #contains a column named "sdasdsds" which has the same value as "Instagram user"
 data4 = readr::read_csv("C:\\Users\\Yiwen\\Documents\\parisjoin4.csv") #contains the same column "Instagram photo" with one value that is the same to the original one
+
+
+data <- data.frame("x1" = c("A", "B", "D"))
+
+
+colnames(data)[which(names(data) == colname)] <- paste0(colname, class(data[[colname]]))
+
+
+
+cols_in_common <- intersect(colnames(data), colnames(data2))
+orig_type <- sapply(data[, cols_in_common, drop = FALSE], class)
+new_type <- sapply(data2[, cols_in_common, drop = FALSE], class)
+orig_type == new_type
+orig_type
+new_type
