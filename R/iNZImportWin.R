20e332ba095e0a1bf5298ac4c222d6e4218bd5ff
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2019-05-01 11:50:34 +1200

    Only add popup when file extension is csv or txt

--- R/iNZImportWin.R
+++ R/iNZImportWin.R
@@ -321,27 +321,29 @@ iNZImportWin <- setRefClass(
                             )
                         prev <<- gdf(head(tmpData, 5), container = prevGp)
                         invisible(prev$remove_popup_menu())
-                        invisible(prev$add_popup(function(col_index) {
-                            j <- prev$get_column_index(col_index)
-                            types <- c(
-                                "auto", 
-                                "numeric", 
-                                "categorical",
-                                "date",
-                                "time",
-                                "datetime"
-                            )
-                            list(
-                                gradio(types,
-                                    selected = match(fColTypes[j], types),
-                                    handler = function(h, ...) {
-                                        fColTypes[j] <<-
-                                            types[svalue(h$obj, index = TRUE)]
-                                        generatePreview(h, ..., reload = TRUE)
-                                    }
-                                )
-                            )
-                        }))
+                        if (fext %in% c("csv", "txt")) {
+                          invisible(prev$add_popup(function(col_index) {
+                              j <- prev$get_column_index(col_index)
+                              types <- c(
+                                  "auto", 
+                                  "numeric", 
+                                  "categorical",
+                                  "date",
+                                  "time",
+                                  "datetime"
+                              )
+                              list(
+                                  gradio(types,
+                                      selected = match(fColTypes[j], types),
+                                      handler = function(h, ...) {
+                                          fColTypes[j] <<-
+                                              types[svalue(h$obj, index = TRUE)]
+                                          generatePreview(h, ..., reload = TRUE)
+                                      }
+                                  )
+                              )
+                          }))
+                        }
                         names(prev) <<- paste0(
                             names(prev),
                             " (",
