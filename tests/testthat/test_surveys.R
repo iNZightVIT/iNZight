f21db79083b1c8ba5d61d5cc74f66b37229e88f8
Tom Elliott <tomelliottnz@gmail.com>
2019-06-15 09:43:54 +1200

    Debugging appveyor [skip travis]

--- tests/testthat/test_surveys.R
+++ tests/testthat/test_surveys.R
@@ -191,6 +191,7 @@ test_that("Post stratification set by importing additional dataset", {
     )
 
     # read from file
+    print(list.files())
     expect_silent(swin$update_levels(read.csv("poptypes.csv")))
     expect_equal(
         sapply(swin$PSlvls$children[c(4, 6, 8)], svalue),
