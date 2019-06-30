13a9ba5870d100c3971ef96001ebbde57102f5b4
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2019-07-01 11:52:21 +1200

    Check if y is null before changing fill colour

--- R/iNZPlotModWin.R
+++ R/iNZPlotModWin.R
@@ -1726,7 +1726,7 @@ iNZPlotMod <- setRefClass(
                     newSet$palette <- svalue(paletteCombobox)
                   }
                   
-                  if (PLOTTYPE %in% c("gg_violin", "gg_column2", "gg_lollipop", "gg_boxplot", "gg_density", "gg_cumcurve")) {
+                  if (PLOTTYPE %in% c("gg_violin", "gg_column2", "gg_lollipop", "gg_boxplot", "gg_density", "gg_cumcurve") && attr(PLOTTYPES, "null.y")) {
                     newSet$fill_colour <- svalue(colourCombobox)
                   }
                   
