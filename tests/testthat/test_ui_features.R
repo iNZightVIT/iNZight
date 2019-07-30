01d0bc191b09b44a51c55304c19b7632f0cdc518
Tom Elliott <tomelliottnz@gmail.com>
2019-07-30 21:00:28 +1200

    Skip view test during r cmd check - because view spits an error

--- tests/testthat/test_ui_features.R
+++ tests/testthat/test_ui_features.R
@@ -5,5 +5,6 @@ ui$initializeGui(census.at.school.500)
 on.exit(try(ui$close(), TRUE))
 
 test_that("Dataset can be viewed using system viewer", {
+    skip_if(Sys.getenv("_R_CHECK_SCREEN_DEVICE_", "") %in% c("stop", "warn"))
     expect_silent(ui$view_dataset())
 })
