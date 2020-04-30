1e42847b0111f685fb2da897a1f5e4cedef6d8b3
Tom Elliott <tomelliottnz@gmail.com>
2020-04-30 14:33:20 +1200

    Disable addon test on appveyor

--- tests/testthat/test_addons.R
+++ tests/testthat/test_addons.R
@@ -1,5 +1,7 @@
 context("Add-on Modules")
 
+skip_on_appveyor()
+
 moduledir <- file.path(getwd(), "modules")
 
 # load_all("../../../iNZightModules")
@@ -7,35 +9,35 @@ moduledir <- file.path(getwd(), "modules")
 
 # try(ui$close())
 ui <- iNZGUI$new()
-ui$initializeGui(iris)#, addonDir = moduledir)
+ui$initializeGui(iris, addonDir = moduledir)
 
 test_that("Modules loaded into menu", {
-    # advmenu <- svalue(ui$menuBarWidget$menubar)$Advanced
-    # expect_true(all(
-    #     c("DemoModule", "DemoModule2", "DemoModule3") %in% names(advmenu)
-    # ))
+    advmenu <- svalue(ui$menuBarWidget$menubar)$Advanced
+    expect_true(all(
+        c("DemoModule", "DemoModule2", "DemoModule3") %in% names(advmenu)
+    ))
 })
 
-# test_that("Modules launch and close", {
-#     advmenu <- svalue(ui$menuBarWidget$menubar)$Advanced
-#     DM <- advmenu[["DemoModule"]]
-#     expect_equal(DM$widget$label, "Demo Module")
-#     expect_output(
-#         modwin <- DM$handler_id$o$o(NULL),
-#         "Running new module"
-#     )
-#     expect_is(modwin, "Demo Module")
-#     expect_equivalent(modwin$get_data(), iris)
-#     expect_output(
-#         modwin$close(),
-#         "Closing module"
-#     )
-# })
+test_that("Modules launch and close", {
+    advmenu <- svalue(ui$menuBarWidget$menubar)$Advanced
+    DM <- advmenu[["DemoModule"]]
+    expect_equal(DM$widget$label, "Demo Module")
+    expect_output(
+        modwin <- DM$handler_id$o$o(NULL),
+        "Running new module"
+    )
+    expect_is(modwin, "Demo Module")
+    expect_equivalent(modwin$get_data(), iris)
+    expect_output(
+        modwin$close(),
+        "Closing module"
+    )
+})
 
-# test_that("Add/remove modules window", {
-#     # addrm <- svalue(ui$menuBarWidget$menubar)$Advanced$install
-#     # expect_is(addrm, "GAction")
-#     ## more tests later
-# })
+test_that("Add/remove modules window", {
+    # addrm <- svalue(ui$menuBarWidget$menubar)$Advanced$install
+    # expect_is(addrm, "GAction")
+    ## more tests later
+})
 
 ui$close()
