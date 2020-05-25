6fe151521f8977ee1e8b4b35e589e24a91af3df2
Tom Elliott <tomelliottnz@gmail.com>
2020-05-26 10:56:53 +1200

    Option to save interactive assets locally:

---
+++ tests/testthat/test_save_plot.R
@@ -0,0 +1,27 @@
+context("Save plot")
+
+# try(ui$close(), TRUE); load_all()
+ui <- iNZGUI$new()
+ui$initializeGui(census.at.school.500)
+on.exit(gWidgets2::dispose(ui$win))
+
+test_that("Interactive graphs can be saved 100% locally", {
+    ui$ctrlWidget$V1box$set_value("height")
+    ui$ctrlWidget$V2box$set_value("armspan")
+
+    w <- ui$plotWidget$savePlot()
+    expect_true(w$children[[1]]$children[[1]]$children[[4]]$set_value(tempdir()))
+    expect_true(w$children[[1]]$children[[1]]$children[[7]]$set_value("test"))
+    on.exit(unlink(file.path(tempdir(), "test.html")))
+    on.exit(unlink(file.path(tempdir(), "assets"), TRUE, TRUE), add = TRUE)
+
+    gg <- w$children[[1]]$children[[2]]
+    expect_false(visible(gg))
+    w$children[[1]]$children[[1]]$children[[2]]$set_index(7L)
+    expect_true(visible(gg))
+
+    gg$children[[4]]$set_value(TRUE)
+
+    w$children[[1]]$children[[4]]$children[[2]]$invoke_change_handler()
+    expect_true(dir.exists(file.path(tempdir(), "assets")))
+})
