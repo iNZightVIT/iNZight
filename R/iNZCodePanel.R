e069619d579279f64815fb420e42ee2b893c8bdf
Tom Elliott <tomelliottnz@gmail.com>
2020-06-05 20:22:04 +1200

    Ability to edit code for the plot - and run it!

--- R/iNZCodePanel.R
+++ R/iNZCodePanel.R
@@ -8,14 +8,16 @@ iNZCodePanel <- setRefClass(
         code_font = "list",
         store_btn = "ANY",
         run_btn = "ANY",
-        reset_btn = "ANY"
+        reset_btn = "ANY",
+        original_code = "ANY"
     ),
     methods = list(
         initialize = function(gui) {
             initFields(
                 GUI = gui,
                 button_width = 80, button_height = 25,
-                code_font = list(family = "monospace", size = 10)
+                code_font = list(family = "monospace", size = 10),
+                original_code = ""
             )
             panel <<- gvbox()
             panel$set_borderwidth(5)
@@ -33,7 +35,7 @@ iNZCodePanel <- setRefClass(
 
             ctrl_pnl <- ggroup(container = panel, expand = TRUE, fill = TRUE)
 
-            lbl <- glabel("R code for the current plot is shown above")
+            lbl <- glabel("R code for the current plot is shown above, which can be edited and run.")
             font(lbl) <- list(size = 9, weight = "bold")
             add(ctrl_pnl, lbl, anchor = c(-1, 0))
 
@@ -45,11 +47,11 @@ iNZCodePanel <- setRefClass(
                 handler = function(h, ...) store_code()
             )
             run_btn <<- gbutton("Run",
-                # container = btn_pnl,
+                container = btn_pnl,
                 handler = function(h, ...) run_code()
             )
             reset_btn <<- gbutton("Reset",
-                # container = btn_pnl,
+                container = btn_pnl,
                 handler = function(h, ...) reset_code()
             )
             store_btn$set_icon("rlogo")
@@ -62,21 +64,81 @@ iNZCodePanel <- setRefClass(
             font(run_btn) <<- list(size = 9)
             font(reset_btn) <<- list(size = 9)
 
+            enabled(store_btn) <<- enabled(run_btn) <<- enabled(reset_btn) <<-
+                svalue(input) != ""
+
             size(panel) <<- c(-1, 90)
         },
         set_input = function(code) {
+            original_code <<- code
             svalue(input) <<- ""
             insert(input, code, where = "beginning", font.attr = code_font)
-            input
+            enabled(store_btn) <<- enabled(run_btn) <<- enabled(reset_btn) <<-
+                svalue(input) != ""
         },
         store_code = function() {
             GUI$rhistory$add(attr(GUI$curPlot, "code"))
         },
         run_code = function() {
+            # set code environment
+            assign(
+                GUI$dataNameWidget$datName,
+                GUI$getActiveData(),
+                GUI$code_env
+            )
 
+            if (!is.null(GUI$getActiveDoc()$getModel()$dataDesign)) {
+                assign(
+                    GUI$getActiveDoc()$getModel()$dataDesignName,
+                    GUI$getActiveDoc()$getModel()$createSurveyObject(),
+                    GUI$code_env
+                )
+            }
+
+            tryCatch(
+                {
+                    rawpl <- eval(
+                        parse(text = svalue(input)),
+                        envir = GUI$code_env
+                    )
+                },
+                error = function(e) {
+                    cat("There was an error ... \n")
+                    print(e)
+                },
+                finally = {
+                    if (inherits(rawpl, "ggplot")) print(rawpl)
+
+                    curpl <- unclass(rawpl)
+                    if (!is.null(attr(curpl, "dotplot.redraw")))
+                        if (attr(curpl, "dotplot.redraw"))
+                            rawpl <- eval(
+                                parse(text = svalue(input)),
+                                envir = GUI$code_env
+                            )
+
+                    ### This can be activated once the UI can be reconfigured based on
+                    ### what the user types ... if ever!!
+                    # if (inherits(p, "inzplotoutput") || inherits(p, "ggplot"))
+                    #     GUI$curPlot <<- unclass(rawpl)
+
+                    # if (!is.null(attr(GUI$curPlot, "dotplot.redraw")))
+                    #     if (attr(curPlot, "dotplot.redraw"))
+                    #         GUI$curPlot <<- unclass(
+                    #             rawpl <- eval(
+                    #                 parse(text = svalue(input)),
+                    #                 envir = GUI$code_env
+                    #             )
+                    #         )
+                    # attr(GUI$curPlot, "code") <<- svalue(input)
+                    # enabled(GUI$plotToolbar$exportplotBtn) <<- can.interact(rawpl)
+                    # GUI$plotType <<- attr(GUI$curPlot, "plottype")
+                }
+            )
         },
         reset_code = function() {
-
+            set_input(original_code)
+            run_code()
         }
     )
 )
