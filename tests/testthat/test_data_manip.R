0d8e1d50463a76945c1e80acb148d79b4442cbd0
Tom Elliott <tomelliottnz@gmail.com>
2020-06-30 10:00:15 +1200

    Fix #321

--- tests/testthat/test_data_manip.R
+++ tests/testthat/test_data_manip.R
@@ -13,5 +13,30 @@ test_that("Data Maid report", {
 test_that("Subsetting and reordering columns", {
     # try(gWidgets2::dispose(ui$modWin), TRUE); load_all()
     w <- iNZReorderVarsWin$new(ui)
+    on.exit(try(gWidgets2::dispose(ui$modWin), TRUE))
+})
 
+test_that("Filtering data leaves code OK", {
+    ui$ctrlWidget$V1box$set_value("height")
+    ui$ctrlWidget$V2box$set_value("armspan")
+    ui$getActiveDoc()$setSettings(list(colby = as.name("gender")))
+    expect_match(
+        svalue(ui$code_panel$input),
+        "iNZPlot(height ~ armspan, colby = gender, data = data)",
+        fixed = TRUE
+    )
+
+    w <- iNZFilterWin$new(ui)
+    gWidgets2::dispose(ui$modWin)
+    w$opt1()
+    ui$modWin$children[[1]]$children[[1]]$children[[2]]$set_value("gender")
+    svalue(ui$modWin$children[[1]]$children[[2]]) <- "male"
+    expect_silent(
+        ui$modWin$children[[1]]$children[[3]]$children[[1]]$invoke_change_handler()
+    )
+    expect_match(
+        svalue(ui$code_panel$input),
+        "iNZPlot(height ~ armspan, colby = gender, data = data.filtered)",
+        fixed = TRUE
+    )
 })
