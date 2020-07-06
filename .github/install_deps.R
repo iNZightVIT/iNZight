2fba54149c854fa8a69a1e00ab648c4b398c3397
Tom Elliott <tomelliottnz@gmail.com>
2020-07-07 08:58:39 +1200

    First attempt checking windows/linux via githubactions

---
+++ .github/install_deps.R
@@ -0,0 +1,30 @@
+# R script
+github_deps <- c(
+    "iNZightVIT/gWidgets2RGtk2@inz",
+    "iNZightVIT/iNZightTools@release/1.9",
+    "iNZightVIT/iNZightTS@dev",
+    "iNZightVIT/iNZightMR@dev",
+    "iNZightVIT/iNZightPlots@release/2.12",
+    "iNZightVIT/iNZightModules@dev"
+)
+
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+if (.Platform$OS.type == "windows") {
+    utils::install.packages("XML")
+    if (!requireNamespace("XML")) {
+        download.file(
+            sprintf(
+                "https://cran.r-project.org/bin/windows/contrib/%s/XML_3.99-0.3.zip",
+                paste(strsplit(as.character(getRversion()), "\\.")[[1]][1:2], collapse = ".")
+            ),
+            "xml.zip"
+        )
+        unzip("xml.zip", exdir = .libPaths()[1])
+        unlink("xml.zip")
+    }
+}
+
+remotes::install_github(github_deps)
+remotes::install_deps(dependencies = TRUE)
+remotes::install_cran("rcmdcheck")
---
+++ .github/install_gtk.R
@@ -0,0 +1,8 @@
+install.packages("RGtk2")
+
+if (! dir.exists( file.path( system.file("RGtk2"), "gtk" ) ) ) {
+    url <- "http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
+    download.file(url, "gtk.zip")
+    unzip("gtk.zip", exdir = system.file("RGtk2"))
+    unlink("gtk.zip")
+}
