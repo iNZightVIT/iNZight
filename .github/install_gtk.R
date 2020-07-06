fb0a4896c5fb8a8d13519287c38513b23f6f3e37
Tom Elliott <tomelliottnz@gmail.com>
2020-07-07 10:45:55 +1200

    Try installing to the right place. ..

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -8,10 +8,12 @@ if (! dir.exists(gtkpath)) stop("No directory ...")
 gtkdir <- file.path(gtkpath, "gtk")
 if (! dir.exists( gtkdir ) ) {
     dir.create(gtkdir)
+    cd <- setwd(gtkdir)
     url <- "http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
     download.file(url, "gtk.zip")
-    system("7z x gtk.zip > nul", exdir = gtkdir)
+    system("7z x gtk.zip > nul")
     unlink("gtk.zip")
+    setwd(cd)
 }
 
 print(list.files(gtkpath))
