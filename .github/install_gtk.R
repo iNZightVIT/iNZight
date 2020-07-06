10bb8b4e97a1ad76841cb80eb99f944910c2ca35
Tom Elliott <tomelliottnz@gmail.com>
2020-07-07 09:58:17 +1200

    Specify rgkt2 dir more specifically ..

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -2,11 +2,12 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
 
 install.packages("RGtk2")
 
-print(system.file("RGtk2"))
-print(.libPaths())
-if (! dir.exists( file.path( system.file("RGtk2"), "gtk" ) ) ) {
+gtkpath <- file.path(.libPaths()[1], "RGtk2")
+if (! dir.exists(gtkpath)) stop("No directory ...")
+
+if (! dir.exists( file.path( gtkpath, "gtk" ) ) ) {
     url <- "http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
     download.file(url, "gtk.zip")
-    unzip("gtk.zip", exdir = system.file("RGtk2"))
+    unzip("gtk.zip", exdir = gtkpath)
     unlink("gtk.zip")
 }
