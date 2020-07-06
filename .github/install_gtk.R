daa8c94374d338c89dc011d3f6d76004e6c1c38f
Tom Elliott <tomelliottnz@gmail.com>
2020-07-07 11:44:05 +1200

    Update install_gtk.R

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -8,13 +8,11 @@ if (! dir.exists(gtkpath)) stop("No directory ...")
 gtkdir <- file.path(gtkpath, "gtk")
 if (! dir.exists( gtkdir ) ) {
     dir.create(gtkdir)
-    cd <- setwd(gtkdir)
     url <- "http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
     download.file(url, "gtk.zip")
     system("7z l gtk.zip")
-    system("7z x gtk.zip")
+    system(sprintf("7z x gtk.zip -o%s", gtkdir))
     unlink("gtk.zip")
-    setwd(cd)
 }
 
 print(list.files(gtkpath))
