ef6c1d89aea70461b15f50a5b0d2e24972ff8725
Tom Elliott <tomelliottnz@gmail.com>
2020-07-08 11:27:17 +1200

    Rename directory correctly

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -5,8 +5,8 @@ Rscript -e "install.packages('RGtk2', repos = 'https://cloud.r-project.org')"
 
 echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
-md x64
-7z x gtk.zip -ox64 > nul
+md gtk
+7z x gtk.zip -ogtk > nul
 del gtk.zip
 mv gtk D:\a\_temp\Library\RGtk2\
 
