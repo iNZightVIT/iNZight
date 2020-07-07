434de97a3b8376f564b83cfc63be68e83d4cbbe4
Tom Elliott <tomelliottnz@gmail.com>
2020-07-08 09:14:06 +1200

    Reenable ngrok..

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -5,5 +5,5 @@ echo "Installing gtk ..."
 curl -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
 md gtk
 7z x gtk.zip > nul
-del gtk.zip
+REM del gtk.zip
 mv gtk D:\a\_temp\Library\RGtk2\
