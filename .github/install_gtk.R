222b4e94fff93c33d27ce787f22423a0ffb53339
Tom Elliott <tomelliottnz@gmail.com>
2020-07-07 14:52:54 +1200

    Install 32 and 64bit GTK

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -6,12 +6,17 @@ gtk_path <- file.path(system.file(package = "RGtk2"), "gtk", .Platform$r_arch)
 if (! dir.exists( gtk_path ) ) {
     dir.create(gtk_path, recursive = TRUE)
 
-    url <- "http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
-    path <- file.path(tempdir(), basename(sub("\\?.*", "", url)))
-    download.file(url, path, mode = "wb")
+    url32 <- "http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.22/gtk+-bundle_2.22.1-20101227_win32.zip"
+    url64 <- "http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
 
-    unzip(path, exdir = gtk_path)
-    RGtk2:::.configure_gtk_theme("MS-Windows")
+    for (url in c(url32, url64)) {
+        path <- file.path(tempdir(), basename(sub("\\?.*", "", url)))
+        download.file(url, path, mode = "wb")
 
-    unlink(path)
+        unzip(path, exdir = gtk_path)
+        unlink(path)
+    }
 }
+
+library(RGtk2)
+RGtk2:::.configure_gtk_theme("MS-Windows")
