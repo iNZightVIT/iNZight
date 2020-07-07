5e7b7804bb192692c918cd27d7549432e18b4516
Tom Elliott <tomelliottnz@gmail.com>
2020-07-08 09:41:28 +1200

    Extract to gtk folder!

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -4,6 +4,6 @@ Rscript -e "install.packages('RGtk2', repos = 'https://cloud.r-project.org')"
 echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
 md gtk
-7z x gtk.zip > nul
+7z x gtk.zip -ogtk > nul
 REM del gtk.zip
 mv gtk D:\a\_temp\Library\RGtk2\
