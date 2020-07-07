9280e34d3c36f836e01726d134a19fe593f18e70
Tom Elliott <tomelliottnz@gmail.com>
2020-07-07 12:54:32 +1200

    Typo :)

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -3,7 +3,7 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
 install.packages("RGtk2")
 
 gtk_path <- file.path(system.file(package = "RGtk2"), "gtk", .Platform$r_arch)
-if (! dir.exists( gtkdir ) ) {
+if (! dir.exists( gtk_path ) ) {
     dir.create(gtk_path, recursive = TRUE)
 
     url <- "http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
