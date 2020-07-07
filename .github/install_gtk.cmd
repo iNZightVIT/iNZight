b7fb5ae0c93341b5fd80594f39a05d3f729f014f
Tom Elliott <tomelliottnz@gmail.com>
2020-07-08 11:05:36 +1200

    Unzip everything to file, then move

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -7,11 +7,12 @@ echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
 md gtk
 7z x gtk.zip -ogtk > nul
-REM del gtk.zip
-mv gtk D:\a\_temp\Library\RGtk2\
+del gtk.zip
+REM mv gtk D:\a\_temp\Library\RGtk2\
 
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.22/gtk+-bundle_2.22.1-20101227_win32.zip
 md gtk
 7z x gtk.zip -ogtk > nul
-REM del gtk.zip
+del gtk.zip
+
 mv gtk D:\a\_temp\Library\RGtk2\
