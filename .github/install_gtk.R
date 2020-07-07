fe2dd378df27ef3e0527982168c70ca07977576c
Tom Elliott <tomelliottnz@gmail.com>
2020-07-07 12:41:38 +1200

    Use RGtk2 helper functions

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -2,15 +2,16 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
 
 install.packages("RGtk2")
 
-gtkpath <- file.path(.libPaths()[1], "RGtk2")
-if (! dir.exists(gtkpath)) stop("No directory ...")
-
-gtkdir <- file.path(gtkpath, "gtk")
+gtk_path <- RGtk2:::.windows_gtk_path()
 if (! dir.exists( gtkdir ) ) {
-    dir.create(gtkdir)
+    dir.create(gtk_path, recursive = TRUE)
+
     url <- "http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip"
-    download.file(url, "gtk.zip")
-    system("7z l gtk.zip")
-    system(sprintf("7z x gtk.zip -o%s", gtkdir))
-    unlink("gtk.zip")
+    path <- file.path(tempdir(), basename(sub("\\?.*", "", url)))
+    download.file(url, path, mode = "wb")
+
+    unzip(path, exdir = gtk_path)
+    RGtk2:::.configure_gtk_theme("MS-Windows")
+
+    unlink(path)
 }
