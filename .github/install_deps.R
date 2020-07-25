b55016c8591c1ae08e32abb69ea25686ab710546
Tom Elliott <tomelliottnz@gmail.com>
2020-07-26 09:06:24 +1200

    Fix install order; install rodbc binary

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -2,20 +2,24 @@
 github_deps <- c(
     "iNZightVIT/gWidgets2RGtk2@inz",
     "iNZightVIT/iNZightTools@1.9",
-    "iNZightVIT/iNZightRegression@1.2.8",
     "iNZightVIT/iNZightTS@1.5.4",
     "iNZightVIT/iNZightMR@2.2.5",
     "iNZightVIT/iNZightPlots@2.12",
+    "iNZightVIT/iNZightRegression@1.2.8",
     "iNZightVIT/iNZightModules@2.5.3"
 )
 
 options(
     repos = c(
-        RSPM = Sys.getenv("RSPM"),
+        if (RSPM = Sys.getenv("RSPM"),
         CRAN = "https://cloud.r-project.org"
     ),
     install.packages.compile.from.source = "never"
 )
+
+if (.Platform$OS.type == "windows" && getRversion() < numeric_version("4")) {
+    install.packages("RODBC", type = "binary")
+}
 
 remotes::install_github(
     github_deps,
