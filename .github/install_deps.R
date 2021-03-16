9972c6c2e5412f625c4faee26a70ec897e45d2bb
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-03-16 13:30:50 +1300

    switch to iNZightTools@dev

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -1,7 +1,7 @@
 # R script
 github_deps <- c(
     "iNZightVIT/gWidgets2RGtk2@inz",
-    "iNZightVIT/iNZightTools@feature/form-class-intervals",
+    "iNZightVIT/iNZightTools@dev",
     "iNZightVIT/iNZightTS@1.5.4",
     "iNZightVIT/iNZightMR@2.2.5",
     "iNZightVIT/iNZightPlots@2.12",
