5bbeae89c36b749611c9e164e6cb234ef0fc01b3
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-04-09 14:14:25 +1200

    remove old scripts dir

--- scripts/data.R
+++
@@ -1,9 +0,0 @@
-## Script to create(/update) datasets shipped with iNZight
-
-SURFIncomeSurvey_200 <- read.csv("~/Downloads/SURFIncomeSurvey.csv", header = TRUE, comment.char = "#")
-SURFIncomeSurvey_200$X <- NULL
-devtools::use_data(SURFIncomeSurvey_200, overwrite = TRUE)
-
-
-devtools::load_all()
-devtools::document()
