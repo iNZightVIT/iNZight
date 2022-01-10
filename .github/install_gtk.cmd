3ce0d187ae2cc37fb15d8105559ec4f8224ab1c9
Tom Elliott <tom.elliott@auckland.ac.nz>
2022-01-10 13:25:37 +1300

    Install RGtk2 from inzight cran

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -1,7 +1,7 @@
 if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
 
 echo "Installing RGtk2 ..."
-Rscript -e "install.packages('RGtk2', repos = 'https://cloud.r-project.org')"
+Rscript -e "install.packages('RGtk2', repos = 'https://r.docker.stat.auckland.ac.nz')"
 
 echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
