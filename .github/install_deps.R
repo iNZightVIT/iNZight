87e2cef653d16b1f03c93c550981571b82696b21
Tom Elliott <tomelliottnz@gmail.com>
2022-05-03 12:33:35 +1200

    update github actions to install feature deps

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -2,10 +2,10 @@
 github_deps <- c(
     "tmelliott/gWidgets2@patch-1",
     "iNZightVIT/gWidgets2RGtk2@inz",
-    "iNZightVIT/iNZightTools@dev",
+    "iNZightVIT/iNZightTools@feature/guinz",
     "iNZightVIT/iNZightTS@1.5.4",
     "iNZightVIT/iNZightMR@2.2.5",
-    "iNZightVIT/iNZightPlots@2.14",
+    "iNZightVIT/iNZightPlots@feature/guinz",
     "iNZightVIT/iNZightRegression@1.2.8",
     "iNZightVIT/iNZightModules@2.5.3"
 )
