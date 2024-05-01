c112867a3ddd43ffe595425968cd9f749985fe35
Tom Elliott <tomelliottnz@gmail.com>
2024-05-02 08:22:36 +1200

    use pak

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -19,59 +19,67 @@ options(
         CRAN = "https://cloud.r-project.org"
     )
     # install.packages.compile.from.source = "never"
-)
+# )
 
-if (OS == "Windows" && getRversion() < numeric_version("4")) {
-    install.packages("RODBC", type = "binary")
-}
-if (OS == "Windows" && !requireNamespace("utf8", quietly = TRUE)) {
-    install.packages("utf8", type = "source")
-}
-if (OS == "Windows" && packageVersion("Matrix") < "1.6-0") {
-    install.packages("Matrix", type = "source")
-}
-# rlang >= 1.1.3
-if (OS == "Windows" &&
-    (!requireNamespace("rlang", quietly = TRUE) ||
-        packageVersion("rlang") < "1.1.3")
-) {
-    install.packages("rlang", type = "source")
-}
-if (OS == "Linux") {
-    remotes::install_github("lawremi/RGtk2",
-        subdir = "RGtk2",
-        dependencies = TRUE,
-        INSTALL_opts = "--no-multiarch"
-    )
-    remotes::install_github("tmelliott/cairoDevice",
-        dependencies = TRUE,
-        INSTALL_opts = "--no-multiarch"
-    )
-    install.packages(
-        c("gWidgets2", "gWidgets2RGtk2"),
-        repos = c(getOption("repos"), "https://r.docker.stat.auckland.ac.nz")
-    )
+# if (OS == "Windows" && getRversion() < numeric_version("4")) {
+#     install.packages("RODBC", type = "binary")
+# }
+# if (OS == "Windows" && !requireNamespace("utf8", quietly = TRUE)) {
+#     install.packages("utf8", type = "source")
+# }
+# if (OS == "Windows" && packageVersion("Matrix") < "1.6-0") {
+#     install.packages("Matrix", type = "source")
+# }
+# # rlang >= 1.1.3
+# if (OS == "Windows" &&
+#     (!requireNamespace("rlang", quietly = TRUE) ||
+#         packageVersion("rlang") < "1.1.3")
+# ) {
+#     install.packages("rlang", type = "source")
+# }
+# if (OS == "Linux") {
+#     remotes::install_github("lawremi/RGtk2",
+#         subdir = "RGtk2",
+#         dependencies = TRUE,
+#         INSTALL_opts = "--no-multiarch"
+#     )
+#     remotes::install_github("tmelliott/cairoDevice",
+#         dependencies = TRUE,
+#         INSTALL_opts = "--no-multiarch"
+#     )
+#     install.packages(
+#         c("gWidgets2", "gWidgets2RGtk2"),
+#         repos = c(getOption("repos"), "https://r.docker.stat.auckland.ac.nz")
+#     )
+# }
+
+# if (OS == "Windows") {
+#     if (!requireNamespace("estimability", quietly = TRUE)) {
+#         remotes::install_version("estimability", version = "1.4.1")
+#     }
+#     if (!requireNamespace("vctrs", quietly = TRUE)) {
+#         install.packages("vctrs", type = "source")
+#     }
+#     if (!requireNamespace("R.utils", quietly = TRUE)) {
+#         install.packages("R.utils")
+#     }
+# }
+
+if (!requireNamespace("pak", quietly = TRUE)) {
+    install.packages("pak", type = "source")
 }
 
-if (OS == "Windows") {
-    if (!requireNamespace("estimability", quietly = TRUE)) {
-        remotes::install_version("estimability", version = "1.4.1")
-    }
-    if (!requireNamespace("vctrs", quietly = TRUE)) {
-        install.packages("vctrs", type = "source")
-    }
-    if (!requireNamespace("R.utils", quietly = TRUE)) {
-        install.packages("R.utils")
-    }
-}
+pak::pak(github_deps, dependencies = TRUE)
+pak::local_install_deps(dependencies = TRUE)
+pak::pak("rcmdcheck")
 
-remotes::install_github(
-    github_deps,
-    dependencies = TRUE,
-    INSTALL_opts = "--no-multiarch"
-)
-remotes::install_deps(
-    dependencies = TRUE,
-    INSTALL_opts = "--no-multiarch"
-)
-remotes::install_cran("rcmdcheck")
+# remotes::install_github(
+#     github_deps,
+#     dependencies = TRUE,
+#     INSTALL_opts = "--no-multiarch"
+# )
+# remotes::install_deps(
+#     dependencies = TRUE,
+#     INSTALL_opts = "--no-multiarch"
+# )
+# remotes::install_cran("rcmdcheck")
