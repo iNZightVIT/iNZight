8cdd8afc78bfc6cedb5af83c7baff1c954b0bfc9
Tom Elliott <tomelliottnz@gmail.com>
2024-05-02 10:21:31 +1200

    no install map pkgs

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -2,9 +2,6 @@
 github_deps <- c(
     "tmelliott/surveyspec",
     "tmelliott/gWidgets2@patch-1",
-    "cran/maptools",
-    "cran/rgeos",
-    "cran/rgdal",
     "iNZightVIT/gWidgets2RGtk2@inz",
     "iNZightVIT/iNZightTools@2.0.0",
     "iNZightVIT/iNZightTS@1.5.10",
