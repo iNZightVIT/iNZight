144e5fb69a0b2732e5987c3dce44c7a30c5af688
Tom Elliott <tomelliottnz@gmail.com>
2024-05-04 08:56:58 +1200

    install GTK and add to path ...

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -1,4 +1,4 @@
-if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
+@REM if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
 
 echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
@@ -6,10 +6,14 @@ md gtk
 7z x gtk.zip -ogtk > nul
 del gtk.zip
 
+mv gtk D:\a\_temp\
+exit 0
+
 if exist D:\a\_temp\Library\RGtk2\NUL (
   mv gtk D:\a\_temp\Library\RGtk2\
   exit 0
 )
+
 
 echo "Installing RGtk2 ..."
 Rscript -e "Sys.setenv(GTK_PATH = file.path(getwd(), 'gtk')); install.packages(c('RGtk2', 'cairoDevice'), repos = 'https://r.docker.stat.auckland.ac.nz', type = 'source')"
