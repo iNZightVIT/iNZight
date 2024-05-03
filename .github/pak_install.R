3ebe952765a15e52c08b54d8b34b19947e2258ac
Tom Elliott <tomelliottnz@gmail.com>
2024-05-04 09:05:52 +1200

    install script

---
+++ .github/pak_install.R
@@ -0,0 +1,8 @@
+# Install/Update packages
+cat("::group::Install/update packages\n")
+Sys.setenv("PKGCACHE_HTTP_VERSION" = "2")
+library(pak, lib.loc = Sys.getenv("R_LIB_FOR_PAK"))
+pak::lockfile_install(".github/pkg.lock")
+## Clean up lock file
+unlink(".github/pkg.lock")
+cat("::endgroup::\n")
