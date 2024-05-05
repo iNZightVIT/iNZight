769d085d883657261bf01d2e136c7ea1eed0d0a1
Tom Elliott <tomelliottnz@gmail.com>
2024-05-06 11:44:24 +1200

    attempt install with path

--- .github/pak_install.R
+++ .github/pak_install.R
@@ -3,7 +3,6 @@ cat("::group::Install/update packages\n")
 Sys.setenv("PKGCACHE_HTTP_VERSION" = "2")
 library(pak, lib.loc = Sys.getenv("R_LIB_FOR_PAK"))
 
-
 pak::lockfile_install(".github/pkg.lock")
 ## Clean up lock file
 unlink(".github/pkg.lock")
