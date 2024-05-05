f2ed4a2f5f47fc88b0902c74338326dc4bf32352
Tom Elliott <tomelliottnz@gmail.com>
2024-05-06 08:47:56 +1200

    typo

--- .github/pak_install.R
+++ .github/pak_install.R
@@ -2,7 +2,7 @@
 cat("::group::Install/update packages\n")
 Sys.setenv("PKGCACHE_HTTP_VERSION" = "2")
 library(pak, lib.loc = Sys.getenv("R_LIB_FOR_PAK"))
-options(repos = c(options$repos, "https://r.docker.stat.auckland.ac.nz"))
+options(repos = c(options()$repos, "https://r.docker.stat.auckland.ac.nz"))
 pak::lockfile_install(".github/pkg.lock")
 ## Clean up lock file
 unlink(".github/pkg.lock")
