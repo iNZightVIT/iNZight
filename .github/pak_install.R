f5bf8b3d524c4552082fe609bc7474cff4665577
Tom Elliott <tomelliottnz@gmail.com>
2024-05-06 10:35:06 +1200

    try install binaries

--- .github/pak_install.R
+++ .github/pak_install.R
@@ -3,8 +3,6 @@ cat("::group::Install/update packages\n")
 Sys.setenv("PKGCACHE_HTTP_VERSION" = "2")
 library(pak, lib.loc = Sys.getenv("R_LIB_FOR_PAK"))
 
-cat("Configured ppm: \n")
-print(pak::repo_status())
 
 pak::lockfile_install(".github/pkg.lock")
 ## Clean up lock file
