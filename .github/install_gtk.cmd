801a6232f0eba9f820328c6bc5426778d126686d
Tom Elliott <tomelliottnz@gmail.com>
2024-05-06 14:12:02 +1200

    install

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -1,4 +1,4 @@
-@REM if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
+if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
 
 echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
@@ -6,15 +6,13 @@ md gtk
 7z x gtk.zip -ogtk > nul
 del gtk.zip
 
-mv gtk D:\a\_temp\
-exit 0
-
-if exist D:\a\_temp\Library\RGtk2\NUL (
+if not exist D:\a\_temp\Library\RGtk2\NUL (
+  echo "Installing RGtk2 ..."
+  Rscript -e "Sys.setenv(GTK_PATH = file.path(getwd(), 'gtk')); try(install.packages(c('RGtk2', 'cairoDevice'), repos = 'https://r.docker.stat.auckland.ac.nz', type = 'source'))"
   mv gtk D:\a\_temp\Library\RGtk2\
-  exit 0
 )
 
-echo "Installing RGtk2 ..."
-Rscript -e "Sys.setenv(GTK_PATH = file.path(getwd(), 'gtk')); try(install.packages(c('RGtk2', 'cairoDevice'), repos = 'https://r.docker.stat.auckland.ac.nz', type = 'source'))"
-
-mv gtk D:\a\_temp\Library\RGtk2\
+echo "Installing cairoDevice ..."
+if not exist D:\a\_temp\Library\cairoDevice\NUL (
+  Rscript -e "Sys.setenv(GTK_PATH = 'D:\a\_temp\Library\RGtk2\gtk'); try(install.packages(c('cairoDevice'), repos = 'https://r.docker.stat.auckland.ac.nz', type = 'source'))"
+)
