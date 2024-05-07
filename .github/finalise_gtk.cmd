0a9eee59872c370873a6a31ca6fcc76369baf48b
Tom Elliott <tomelliottnz@gmail.com>
2024-05-07 12:26:30 +1200

    finalise script

---
+++ .github/finalise_gtk.cmd
@@ -0,0 +1,2 @@
+if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
+mv D:\a\_temp\gtk D:\a\_temp\Library\RGtk2\gtk
--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -7,14 +7,3 @@ md gtk
 del gtk.zip
 
 mv gtk D:\a\_temp\
-
-@REM if not exist D:\a\_temp\Library\RGtk2\NUL (
-@REM   echo "Installing RGtk2 ..."
-@REM   Rscript -e "Sys.setenv(GTK_PATH = file.path(getwd(), 'gtk')); install.packages('RGtk2', repos = 'https://r.docker.stat.auckland.ac.nz', type = 'source', INSTALL_opts = c('--no-multiarch'))"
-@REM   mv gtk D:\a\_temp\Library\RGtk2\
-@REM )
-
-@REM echo "Installing cairoDevice ..."
-@REM if not exist D:\a\_temp\Library\cairoDevice\NUL (
-@REM   Rscript -e "Sys.setenv(GTK_PATH = 'D:/a/_temp/Library/RGtk2/gtk'); install.packages('cairoDevice', repos = 'https://r.docker.stat.auckland.ac.nz', type = 'source', INSTALL_opts = c('--no-multiarch'))"
-@REM )
