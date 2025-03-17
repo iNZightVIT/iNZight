f8ec1536d34b62d50f75b24e44f84a8b91091a26
Tom Elliott <tom@inzight.co.nz>
2025-03-17 15:36:50 +1300

    no multiarch

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -19,11 +19,18 @@ if (!requireNamespace("remotes", quietly = TRUE)) {
 if (!requireNamespace("RGtk2", quietly = TRUE) ||
     packageVersion("RGtk2") < numeric_version("2.20.41")) {
     cat("Installing RGtk2 ...\n")
-    remotes::install_github("tmelliott/RGtk2/RGtk2", type = "source", build = FALSE)
+    remotes::install_github("tmelliott/RGtk2/RGtk2",
+        type = "source",
+        build = FALSE,
+        INSTALL_opts = "--no-multiarch"
+    )
 }
 if (!requireNamespace("cairoDevice", quietly = TRUE)) {
     cat("Installing cairoDevice ...\n")
-    install.packages("cairoDevice", type = "source", build = FALSE)
+    install.packages("cairoDevice",
+        type = "source",
+        build = FALSE, INSTALL_opts = "--no-multiarch"
+    )
 }
 
 if (.Platform$OS.type == "windows" &&
