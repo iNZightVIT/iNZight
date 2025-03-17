6d7c268c5b0c6b285b3b0e49fad5935f5699ffd8
Tom Elliott <tom@inzight.co.nz>
2025-03-17 13:44:08 +1300

    fixes

--- .github/install_gtk.cmd
+++ .github/install_gtk.cmd
@@ -1,13 +1,5 @@
-if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
-
-echo "Installing gtk ..."
+echo "Downloading gtk ..."
 curl -L -o gtk.zip https://inzight.nz/data/gtk+-bundle_2.22.1-20101229_win64.zip
 md gtk
 7z x gtk.zip -ogtk > nul
 del gtk.zip
-
-echo "Installing RGtk2 ..."
-Rscript .github\install_gtk_win.R
-
-echo "Moving gtk to RGtk2 ..."
-mv gtk D:\a\_temp\Library\RGtk2\
