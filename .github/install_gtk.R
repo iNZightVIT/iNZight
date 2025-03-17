327f0b1add3b5d3736c9cca3b2a761be6ffa7d8c
Tom Elliott <tom@inzight.co.nz>
2025-03-17 16:34:48 +1300

    add repos

--- .github/install_gtk.R
+++ .github/install_gtk.R
@@ -19,7 +19,10 @@ if (!requireNamespace("remotes", quietly = TRUE)) {
 if (!requireNamespace("RGtk2", quietly = TRUE)) {
     cat("Installing RGtk2 ...\n")
     if (.Platform$OS.type == "windows") {
-        remotes::install_cran("RGtk2", INSTALL_opts = "--no-test-load")
+        remotes::install_cran("RGtk2",
+            INSTALL_opts = "--no-test-load",
+            repos = "https://r.docker.stat.auckland.ac.nz"
+        )
     } else {
         remotes::install_github("tmelliott/RGtk2/RGtk2",
             type = "source",
